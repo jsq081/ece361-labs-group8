64b3c99397868891afc0554a73008561a8547df1
RoastedZiran <84394286+RoastedZiran@users.noreply.github.com>
2021-09-19 23:47:23 -0400

    Update deliver.c

--- FileTransferLab/Lab1/deliver.c
+++ FileTransferLab/Lab1/deliver.c
@@ -1,71 +1,81 @@
 #include <stdio.h>
+#include <string.h> 
+#include <stdlib.h> 
+#include <arpa/inet.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <sys/types.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <netdb.h>
 
 int main(int argc, char** argv){
+
     if (argc != 3) {
         printf("Error: execution command should be: deliver <server address> <server port number>\n");
         exit(EXIT_FAILURE);
     }
 
-    //1. ask the user to input message
-    printf("Input the message as follows:\n        ftp <file name>\n");
-    char user_input[100] = {'0'};
-    char command[100] = {'0'};
-    char filename[100] = {'0'};
-    //fgets(user_input, 100, stdin); // stdin: read from the standard input
-    scanf("%s", &command);
-    scanf("%s", &filename);
+    int port = atoi(argv[2]);
 
-    if (strcmp(command, "ftp") != 0){
-        printf("Error: the command should be:\n        ftp <file name>\n");
-        exit(EXIT_FAILURE);
+    //create the socket
+    int socketfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (socketfd < 0){
+        printf("Error: unsuccessful creation of socket");
+        exit (EXIT_FAILURE);
     }
 
-    int port = atoi(argv[2]);
-    // server address = argc[1]
-    int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    // convert input string to IP address
-    inet_pton(AF_INET, argv[1], &(server_addr.sin_addr));
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
     // Note that sin_zero (which is included to pad the structure to the length of a struct sockaddr)
     // should be set to all zeros with the function memset(). -- Beej
-    memset(server_addr.sin_zero, 0, sizeof(server_addr.sin_zero));
+    memset (serv_addr.sin_zero, 0, sizeof(serv_addr.sin_zero));
+    // convert input string to IP address
+    inet_pton(AF_INET,argv[1],&(serv_addr.sin_addr));
 
-    socklen_t ser_addrlen = sizeof(server_addr);
+    char input[100]; 
+    char filename[100]; 
+    
 
-    // 2. check the existence of the file
-    if( access(filename, F_OK) == 0 ) {
-        printf("-----File exist, sending ftp to the server...-----\n");
-        if (sendto(socket_fd, "ftp", sizeof("ftp"), 0, (struct sockaddr*) &server_addr, ser_addrlen) == -1){
-            printf("Error: send failed\n");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        printf("File \"%s\" does not exist\n", filename);
+    //get user input 
+    printf("Input a message of the following form\n\t ftp <file name>\n");
+
+    scanf("%s", &input);
+    scanf("%s", &filename);
+
+
+    if(strcmp(input, "ftp") != 0){
+        printf("Error: the command should be:\n        ftp <file name>\n\n");
         exit(EXIT_FAILURE);
     }
 
-    // 3. receive message from server
-    char buffer[100] = {'0'};
-    if (recvfrom(socket_fd, buffer, sizeof(buffer), 0, (struct sockaddr*) &server_addr, &ser_addrlen) == -1){
-        printf("Error: receive failed\n");
+    // check if the file exists
+    if( access( filename, F_OK ) == -1) {
+        printf("File does not exists.\n");
         exit(EXIT_FAILURE);
-    }
-    printf("-----Receiving server message...-----\n");
-    if (strcmp(buffer, "yes") == 0){
-        printf("A file transfer can start.\n");
-    }else {
+    } 
+
+    socklen_t serv_addr_len = sizeof(serv_addr);
+
+    //check if msg send succsessful
+    if (sendto( socketfd, "ftp", sizeof("ftp") , 0 , (struct sockaddr *) &serv_addr, serv_addr_len) == -1) {
+        printf("Failed send msg\n");
         exit(EXIT_FAILURE);
     }
     
+
+    //recv msg 
+    char buf[100];
+    if (recvfrom(socketfd, (char*) buf, sizeof(buf), 0, (struct sockaddr*) &serv_addr, &serv_addr_len) == -1) {
+        printf("Error: Unseccessful recieve\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(strcmp(buf, "yes")==0){
+        printf("A file transfer can start\n");
+    }else{
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
-
