dcbc56812ae0f21c06ddb10ebbea10cf063be984
RoastedZiran <84394286+RoastedZiran@users.noreply.github.com>
2021-09-19 23:56:36 -0400

    Update server.c

--- FileTransferLab/Lab1/server.c
+++ FileTransferLab/Lab1/server.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-#include <string.h> 
-#include <stdlib.h> 
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
+
 
 int main(int argc, char** argv){
 
-    if (argc != 3) {
-        printf("Error: execution command should be: deliver <server address> <server port number>\n");
-        exit(EXIT_FAILURE);
+    //check user argument
+    if(argc != 2){
+        printf("Error: execution command should be:\n server <UDP listen port>\n");
+        exit (EXIT_FAILURE);
     }
 
-    int port = atoi(argv[2]);
+    //convert string to int
+    int port = atoi(argv[1]); 
 
     //create the socket
-    int socketfd = socket(AF_INET, SOCK_DGRAM, 0);
+    int socketfd = socket(AF_INET,SOCK_DGRAM,0);
 
     if (socketfd < 0){
         printf("Error: unsuccessful creation of socket");
@@ -27,55 +32,49 @@ int main(int argc, char** argv){
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
-    // Note that sin_zero (which is included to pad the structure to the length of a struct sockaddr)
-    // should be set to all zeros with the function memset(). -- Beej
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
     memset (serv_addr.sin_zero, 0, sizeof(serv_addr.sin_zero));
-    // convert input string to IP address
-    inet_pton(AF_INET,argv[1],&(serv_addr.sin_addr));
 
-    char input[100]; 
-    char filename[100]; 
-    
-
-    //get user input 
-    printf("Input a message of the following form\n\t ftp <file name>\n");
-
-    scanf("%s", &input);
-    scanf("%s", &filename);
-
-
-    if(strcmp(input, "ftp") != 0){
-        printf("Error: the command should be:\n        ftp <file name>\n\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // check if the file exists
-    if( access( filename, F_OK ) == -1) {
-        printf("File does not exists.\n");
-        exit(EXIT_FAILURE);
-    } 
 
     socklen_t serv_addr_len = sizeof(serv_addr);
-
-    //check if msg send succsessful
-    if (sendto( socketfd, "ftp", sizeof("ftp") , 0 , (struct sockaddr *) &serv_addr, serv_addr_len) == -1) {
-        printf("Failed send msg\n");
-        exit(EXIT_FAILURE);
-    }
-    
-
-    //recv msg 
-    char buf[100];
-    if (recvfrom(socketfd, (char*) buf, sizeof(buf), 0, (struct sockaddr*) &serv_addr, &serv_addr_len) == -1) {
-        printf("Error: Unseccessful recieve\n");
+    // bind socket to address
+    if (bind( socketfd, (const struct sockaddr * ) &serv_addr, serv_addr_len) == -1) {
+        printf("Error: bind error\n");
         exit(EXIT_FAILURE);
     }
 
-    if(strcmp(buf, "yes")==0){
-        printf("A file transfer can start\n");
-    }else{
+    printf("Server receiving on port %d\n", port);
+
+
+    //recieve msg
+    char buf[100];  
+    struct sockaddr_in client_addr; 
+    socklen_t client_len = sizeof(client_addr); // length of client info
+    // recvfrom the client and store info in cli_addr so as to send back later
+    //printf("hahaha\n");
+
+    if (recvfrom(socketfd, (char*) buf, sizeof(buf), 0, (struct sockaddr *) &client_addr, &client_len) == -1) {
+        printf("Error: Unseccessful receive\n");
         exit(EXIT_FAILURE);
     }
+
+
+    printf("Finished receive, start reply\n");
+    //reply msg
+    if (strcmp(buf, "ftp") == 0) {
+        if (sendto(socketfd, "yes", sizeof("yes"), 0, (struct sockaddr *) &client_addr,client_len) == -1) {
+            printf("Error: Unseccessful sendto\n");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        if (sendto(socketfd, "no", sizeof("no"), 0, (struct sockaddr *) &client_addr,client_len) == -1) {
+            printf("Error: Unseccessful sendto\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("Finished reply\n");
+    close(socketfd);
 
     return 0;
 }
